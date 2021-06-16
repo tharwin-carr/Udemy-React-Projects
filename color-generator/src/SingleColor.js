a3bf2c3322d3852aaead6e2e3bf7f630642e6f30
Tharwin <tharwin.carr08@gmail.com>
2021-06-16 12:43:51 -0700

    TC: finished grocery bud app

---
+++ color-generator/src/SingleColor.js
@@ -0,0 +1,15 @@
+import React, { useState, useEffect } from 'react'
+import rgbToHex from './utils'
+
+const SingleColor = ({ rgb, weight, index }) => {
+	const [alert, setAlert] = useState(false)
+	const bcg = rgb.join(',')
+	return (
+		<article
+			className={`color`}
+			style={{ backgroundColor: `rgb(${bcg})` }}
+		></article>
+	)
+}
+
+export default SingleColor
